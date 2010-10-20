f5817ca6907f847bb9f0c396bc71f137767c16a1
Brian Olsen <brian@maven-group.org>
2010-10-20 16:56:38 +0200

    Fixed constantize for Java classes

--- lib/jactive_support.rb
+++ lib/jactive_support.rb
@@ -1,3 +1,4 @@
 require 'jactive_support/core_ext'
 require 'jactive_support/json'
 require 'jactive_support/rescuable'
+require 'jactive_support/constantize'
---
+++ lib/jactive_support/constantize.rb
@@ -0,0 +1,17 @@
+module ActiveSupport
+  module Inflector
+    def constantize_with_jruby(camel_cased_word)
+      names = camel_cased_word.split('::')
+      names.shift if names.empty? || names.first.empty?
+
+      if names.first == "Java" && names.size == 3
+        last = names.pop
+        constantize_without_jruby(names.join('::')).__send__ last
+      else
+        constantize_without_jruby(camel_cased_word)
+      end
+    end
+    alias :constantize_without_jruby :constantize
+    alias :constantize :constantize_with_jruby
+  end
+end
