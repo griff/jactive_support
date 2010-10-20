958de9edb0adb579cd40f303d957aba47e857e65
Brian Olsen <brian@maven-group.org>
2010-10-20 16:57:42 +0200

    Mangled rescuable to support java exceptions.

--- lib/jactive_support/rescuable.rb
+++ lib/jactive_support/rescuable.rb
@@ -3,7 +3,7 @@ module ActiveSupport
   module Rescuable
     
     module ClassMethods
-      def java_rescue_from(*klasses, &block)
+      def rescue_from(*klasses, &block)
         options = klasses.extract_options!
 
         unless options.has_key?(:with)
@@ -15,24 +15,48 @@ module ActiveSupport
         end
 
         klasses.each do |klass|
-          key = if klass.is_a?(Class) && klass <= java.lang.Exception
+          key = if klass.is_a?(Class) && ( klass <= Exception || klass <= ::Java::JavaLang::Exception )
             klass.name
           elsif klass.is_a?(String)
             klass
           else
-            raise ArgumentError, "#{klass} is neither an java.lang.Exception nor a String"
+            raise ArgumentError, "#{klass} is neither an Exception nor a String"
           end
 
           # put the new handler at the end because the list is read in reverse
-          java_rescue_handlers << [key, options[:with]]
+          self.rescue_handlers += [[key, options[:with]]]
         end
       end
     end
 
-    def rescue_with_java_handler(exception)
-    end
+    def handler_for_rescue(exception)
+      # We go from right to left because pairs are pushed onto rescue_handlers
+      # as rescue_from declarations are found.
+      _, rescuer = self.class.rescue_handlers.reverse.detect do |klass_name, handler|
+        # The purpose of allowing strings in rescue_from is to support the
+        # declaration of handler associations for exception classes whose
+        # definition is yet unknown.
+        #
+        # Since this loop needs the constants it would be inconsistent to
+        # assume they should exist at this point. An early raised exception
+        # could trigger some other handler and the array could include
+        # precisely a string whose corresponding constant has not yet been
+        # seen. This is why we are tolerant to unknown constants.
+        #
+        # Note that this tolerance only matters if the exception was given as
+        # a string, otherwise a NameError will be raised by the interpreter
+        # itself when rescue_from CONSTANT is executed.
+        klass = self.class.const_get(klass_name) rescue nil
+        klass ||= klass_name.constantize rescue nil
+        exception.is_a?(klass) || (exception.is_a?(NativeException) && exception.cause.is_a?(klass)) if klass
+      end
 
-    def java_handler_for_rescue(exception)
+      case rescuer
+      when Symbol
+        method(rescuer)
+      when Proc
+        rescuer.bind(self)
+      end
     end
   end
 end
