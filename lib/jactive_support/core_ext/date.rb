dcc657d68a5abea4a4b265415304fb724bcbaf46
Brian Olsen <brian@maven-group.org>
2010-11-26 01:22:04 +0100

    Added converter to java date to all ruby date/time classes

---
+++ lib/jactive_support/core_ext/date.rb
@@ -0,0 +1,20 @@
+class Date
+  def to_java_date
+    java.util.Date.new((to_time.tv_sec*1000).to_i)
+  end
+end
+class Time
+  def to_java_date
+    java.util.Date.new((tv_sec*1000).to_i)
+  end
+end
+class ActiveSupport::TimeWithZone
+  def to_java_date
+    java.util.Date.new((tv_sec*1000).to_i)
+  end
+end
+class Java::JavaUtil::Date
+  def to_java_date
+    self
+  end
+end
