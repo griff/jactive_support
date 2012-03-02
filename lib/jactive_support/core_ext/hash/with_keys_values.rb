745f9ebdfc728b2fc72be7314fcf36cad3f49b64
Brian Olsen <brian@maven-group.org>
2012-03-02 16:13:07 +0100

    Fixed spelling in error message

--- lib/jactive_support/core_ext/hash/with_keys_values.rb
+++ lib/jactive_support/core_ext/hash/with_keys_values.rb
@@ -1,6 +1,6 @@
 class Hash
   def Hash.with_keys_values(keys, values)
-    raise ArgumentError, "Unmathing sizes #{keys.size} != #{values.size}" unless keys.size == values.size
+    raise ArgumentError, "Unmatching sizes #{keys.size} != #{values.size}" unless keys.size == values.size
     #Hash[keys.zip(values).flatten]
     ret = Hash.new
     keys.each_with_index{|key, idx| ret[key] = values[idx]}
