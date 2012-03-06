706791d74799472f480be759bea3b47e9874ceda
Brian Olsen <brian@maven-group.org>
2012-03-06 14:25:08 +0100

    Fixed wrong variable name in to_java

--- lib/jactive_support/core_ext/to_java.rb
+++ lib/jactive_support/core_ext/to_java.rb
@@ -1,7 +1,7 @@
 class Object
   def to_java(cls=nil)
     if cls.is_a?(Symbol) && respond_to?("to_java_#{cls}")
-      send "to_java_#{symbol}"
+      send "to_java_#{cls}"
     else
       super
     end
