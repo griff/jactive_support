9b6c6392b3752b514531d95c56027f9442c75a54
Brian Olsen <brian@maven-group.org>
2012-03-06 14:43:32 +0100

    Added frozen support to extension to iterable

--- lib/jactive_support/java_ext/iterable.rb
+++ lib/jactive_support/java_ext/iterable.rb
@@ -1,7 +1,11 @@
 require 'jactive_support/java_ext/iterator'
 
 module java::lang::Iterable
-  #include Enumerable JRuby already does this
+  if RUBY_VERSION >= '1.9'
+    FrozenError = RuntimeError
+  else
+    FrozenError = TypeError
+  end
   
   def iterate
     return enum_for(:iterate) unless block_given?
@@ -11,17 +15,9 @@ module java::lang::Iterable
       yield(it,ob)
     end
   end
-=begin comment
-  JRuby already contains a similar expression
-  def each
-    it = iterator
-    while it.next?
-      yield it.next
-    end
-  end
-=end
 
   def delete_if
+    raise FrozenError, "can't modify frozen iterable" if frozen?
     return enum_for(:delete_if) unless block_given?
     iterate do |it,ob|
       it.remove if yield(ob)
@@ -30,6 +26,7 @@ module java::lang::Iterable
   end
   
   def reject!
+    raise FrozenError, "can't modify frozen iterable" if frozen?
     return enum_for(:reject!) unless block_given?
     ret = nil
     iterate do |it,ob|
@@ -42,6 +39,7 @@ module java::lang::Iterable
   end
   
   def clear
+    raise FrozenError, "can't modify frozen iterable" if frozen?
     iterate {|it, o| it.remove}
     self
   end
